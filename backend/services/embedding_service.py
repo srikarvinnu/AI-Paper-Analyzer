d0925fb1ee2eff4693181d01660d77e4f5537f1d
Srikar <kannayarao1@gmail.com>
2026-06-11 11:39:11 +0530

    Lazy load embedding model

--- backend/services/embedding_service.py
+++ backend/services/embedding_service.py
@@ -1,12 +1,23 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer(
-    "all-MiniLM-L6-v2"
-)
+model = None
+
+def get_model():
+    global model
+
+    if model is None:
+        model = SentenceTransformer(
+            "all-MiniLM-L6-v2"
+        )
+
+    return model
+
 
 def create_embeddings(chunks):
 
-    embeddings = model.encode(
+    current_model = get_model()
+
+    embeddings = current_model.encode(
         chunks
     )
 
